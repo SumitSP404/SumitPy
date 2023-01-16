08207f6f3cfc9c36e11e6efe63b66b3c057a292b
Sumit S Purandare <122196568+SumitSP404@users.noreply.github.com>
2023-01-16 17:19:46 +0530

    Add files via upload

---
+++ image_to_speech.py
@@ -0,0 +1,39 @@
+import cv2
+#import time
+import pytesseract  
+from gtts import gTTS  
+from pygame import mixer 
+import os 
+
+pytesseract.pytesseract.tessarct_cmd=r"C:\3Vikram\Software\General\Tesseract\tesseract.exe"
+mixer.init()
+def play(text):
+  tts = gTTS(text)
+  tts.save("output.mp3")
+  mixer.music.load('output.mp3')
+  mixer.music.play()
+  while mixer.music.get_busy():
+      pass
+  mixer.music.load('test.mp3')
+  os.remove('output.mp3')
+
+cap = cv2.VideoCapture(0)
+while 1:
+  ret, img = cap.read()
+  img1 = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+  cv2.imshow("Video",img1)
+  text=pytesseract.image_to_string(img1)  
+  print(text)
+  try:
+    play(text)
+  except AssertionError:
+    pass
+
+  #time.sleep(1)
+  if cv2.waitKey(1) == ord("q"):
+    break
+cap.release()
+cv2.destroyAllWindows()
+
+
+
